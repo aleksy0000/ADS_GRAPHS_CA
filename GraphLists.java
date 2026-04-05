7c3b12ae788670d93631468b46f4e152b8293c53
Aleksy Szopinski <alexsszopinski4@gmail.com>
2026-04-05 07:31:44 +0100

    Implemented Adjacency List, DFS, BFS and Heap

--- GraphLists.java
+++ GraphLists.java
@@ -2,13 +2,17 @@
 // Uses an Adjacency Linked Lists, suitable for sparse graphs
 
 import java.io.*;
+import java.util.ArrayDeque;
+import java.util.Queue;
 
 class Heap
 {
-    private int[] a;	   // heap array
-    private int[] hPos;	   // hPos[h[k]] == k
-    private int[] dist;    // dist[v] = priority of v
-
+    //heap array, parent: i / 2, left child: i * 2, right child i * 2 + 1.
+    private int[] a;	
+    // hPos[h[k]] == k, index position relates to vertex i.e., index 3 = vertex C, the stored value relates to the position of vertex C in the heap array a[], i.e, hPos[3] = 5, a[5] == 3
+    private int[] hPos;	    
+    // dist[v] = priority of v, index position relates to vertex, i.e., index 1 = vertex A, the stored value relates to lowest weight edge connecting the vertex to the minimum spanning tree.
+    private int[] dist;    
     private int N;         // heap size
    
     // The heap constructor gets passed from the Graph:
@@ -36,17 +40,46 @@ class Heap
 
         // code yourself
         // must use hPos[] and dist[] arrays
+        while(k > 1 && dist[v] < dist[a[k / 2]]){
+            //swap
+            a[k] = a[k / 2];
+            hPos[a[k]] = k;
+                
+            k = k / 2;
+        }
+
+        a[k] = v;
+        hPos[v] = k;
     }
 
 
-    public void siftDown( int k) 
+    public void siftDown(int k) 
     {
         int v, j;
        
-        v = a[k];  
+        v = a[k]; //assign the initial vertex
         
         // code yourself 
         // must use hPos[] and dist[] arrays
+        // Check if there is atleast a left child
+        while(2 * k <= N){
+            j = 2 * k; //left child, for now it's smallest.
+
+            //compare left child with right
+            if(j < N && dist[a[j + 1]] < dist[a[j]]){
+                j = j + 1; //right child is smaller
+            }
+
+            //swap parent with smallest child
+            a[k] = a[j];
+            hPos[a[k]] = k; 
+
+            k = j;
+        }
+        
+        a[k] = v;
+        hPos[v] = k;
+
     }
 
 
@@ -81,6 +114,8 @@ class Graph {
     // V = number of vertices
     // E = number of edges
     // adj[] is the adjacency lists array
+    // the index of the element in adj is the vertex i.e., vertex 3 is stored at index 3
+    // at index 3, the neighbours of vertex 3 are stored in a linkedlist.
     private int V, E;
     private Node[] adj;
     private Node z;
@@ -118,6 +153,9 @@ class Graph {
         for(v = 1; v <= V; ++v)
             adj[v] = z;               
         
+        // create array for representing vertex visits
+        visited = new int[V + 1];
+
        // read the edges
         System.out.println("Reading edges from text file");
         for(e = 1; e <= E; ++e)
@@ -132,8 +170,21 @@ class Graph {
 
            
             
-            // write code to put edge into adjacency matrix     
-            
+            // write code to put edge into adjacency list     
+            //insert v into u's adjacency list
+            t = new Node();
+            t.vert = v;
+            t.wgt = wgt;
+            t.next = adj[u];
+            adj[u] = t; 
+
+            //insert u into v's adjacency list
+            t = new Node();
+            t.vert = u;
+            t.wgt = wgt;
+            t.next = adj[v];
+            adj[v] = t;
+
         }	       
     }
    
@@ -156,7 +207,74 @@ class Graph {
         System.out.println("");
     }
 
+    public void DF(int s){
+        visited[s] = 1;
+        System.out.println("Visited: " + toChar(s));
+        
+        Node t = adj[s];
 
+        while(t != z){
+            System.out.println();
+            System.out.println("Exploring " + toChar(s));
+            System.out.println();
+
+            int v = t.vert;
+
+            System.out.println("Found " + toChar(s) + "->" + toChar(v));
+
+            if(visited[v] == 0){
+                System.out.println("Going from " + toChar(s) + " to " + toChar(v));
+                DF(v); //recursive call
+                System.out.println("Going back to " + toChar(s) + " from " + toChar(v));
+            }else{
+                System.out.println(toChar(v) + " Already Visited, Moving on to the next vertex adjacent to " + toChar(s));
+            }
+
+            t = t.next;
+        }  
+    }
+
+    public void breadthFirst(int s){
+        Queue<Integer> q = new ArrayDeque<>();
+
+        visited[s] = 1;
+        System.out.println("Visited Initial Vertex " + toChar(s));
+        System.out.println("Inserting initial vertex into queue");
+
+        q.add(s);
+
+        while(!q.isEmpty()){
+            int u = q.remove();
+            System.out.println();
+            System.out.println("Exploring: " + toChar(u));
+            System.out.println();
+
+            Node t = adj[u];
+
+            while(t != z){
+                int v = t.vert;
+                System.out.println("Found " + toChar(u) + "->" + toChar(v));
+
+                if(visited[v] == 0){
+                    System.out.println("Inserting " + toChar(v) + " into queue");
+                    visited[v] = 1;
+                    q.add(v);
+                }
+                
+                displayQueue(q);
+
+                t = t.next;
+            }
+        }
+    }
+
+    public void displayQueue(Queue<Integer> q){
+        System.out.println("Current State of Queue:");
+        for(int x : q){
+            System.out.print(toChar(x) + "->");
+        }
+        System.out.println();
+    }
     
 	public void MST_Prim(int s)
 	{
@@ -200,16 +318,17 @@ class Graph {
 public class GraphLists {
     public static void main(String[] args) throws IOException
     {
-        int s = 2;
+        int s = 12;
         String fname = "wGraph3.txt";               
 
         Graph g = new Graph(fname);
        
         g.display();
 
-       //g.DF(s);
-       //g.breadthFirst(s);
-       //g.MST_Prim(s);   
-       //g.SPT_Dijkstra(s);               
+        g.DF(s);
+        System.out.println("Depth First Traversal Complete");
+        //g.breadthFirst(s);
+        //g.MST_Prim(s);   
+        //g.SPT_Dijkstra(s);               
     }
 }
